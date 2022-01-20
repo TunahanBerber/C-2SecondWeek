22ec3256d3f47460a66f860be7a166a9ff3c6989
Tunahan Berber <83482995+TunahanBerber@users.noreply.github.com>
2022-01-20 18:34:11 +0300

    Add files via upload

---
+++ Donguler/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Donguler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string kurs1 = "Yazılım Geliştirici Yetiştirme Kampı";
+            string kurs2 = "Progrmalamaya başlangıç için temel kurs";
+            string kurs3 = "Java";
+
+            //array-dizi
+
+            string[] kurslar = new string[]
+            {
+                "Yazılım Geliştirici Yetiştirme Kampı",
+                "Progrmalamaya başlangıç için temel kurs",
+                "Java"
+            };
+
+            for (int i = 0; i < kurslar.Length; i++)  //i sayaç anlamına geliyor
+            {
+                Console.WriteLine(kurslar[i]);                
+            }
+
+            Console.WriteLine("For Bitti");
+
+            foreach (string kurs in kurslar)  //kursları dolaş demek
+            {
+                Console.WriteLine(kurs);
+            }
+            
+            Console.WriteLine("Sayfa Sonu - footer");
+        }
+    }
+}
